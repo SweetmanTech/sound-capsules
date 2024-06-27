4707845bee716aaf40c2fe167fef422ccea4d83d
SeniorDev <tech.eng.me@gmail.com>
2024-06-27 16:38:29 -0400

    I update mint with free price

--- apps/site/components/MintSection/MintSection.tsx
+++ apps/site/components/MintSection/MintSection.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
-import WhitelistMintButton from './WhitelistMintButton';
+import FreeMintButton from './FreeMintButton';
 import Quantity from './Quantity';
-import MintButton from './MintButton';
 import useZoraPurchasePresale from '@/hooks/useZoraPurchasePresale';
-import useWhitelist from '@/hooks/useWhitelist';
 
 const MintSection = () => {
 	const { totalSupply } = useZoraPurchasePresale();
 	const [quantity, setQuantity] = useState(1);
-	const { isWhitelisted } = useWhitelist();
 
 	return (
 		<div className="space-y-6">
@@ -17,11 +14,7 @@ const MintSection = () => {
 				setQuantity={setQuantity}
 				totalSupply={totalSupply}
 			/>
-			{isWhitelisted ? (
-				<WhitelistMintButton quantity={quantity} />
-			) : (
-				<MintButton quantity={quantity} />
-			)}
+			<FreeMintButton quantity={quantity} />
 		</div>
 	);
 };
