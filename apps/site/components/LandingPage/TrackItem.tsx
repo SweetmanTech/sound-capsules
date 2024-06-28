938b4079a7f4b1e5e79176c745680f126f05606b
SeniorDev <tech.eng.me@gmail.com>
2024-06-28 10:30:17 -0400

    I remove return

--- apps/site/components/LandingPage/TrackItem.tsx
+++ apps/site/components/LandingPage/TrackItem.tsx
@@ -1,13 +1,11 @@
 import TrackPlayer from "../TrackPlayer"
 import AddTrackButton from "./AddTrackButton"
 
-const TrackItem = ({ track }: any) => {
-  return (
-    <div className="space-y-2 relative">
-      <TrackPlayer track={track} />
-      <AddTrackButton />
-    </div>
-  )
-}
+const TrackItem = ({ track }: any) => (
+  <div className="space-y-2 relative">
+    <TrackPlayer track={track} />
+    <AddTrackButton />
+  </div>
+)
 
 export default TrackItem
