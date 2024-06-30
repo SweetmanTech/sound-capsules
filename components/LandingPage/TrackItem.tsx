58d89ee782049ed44badc78795f2b9ee6be92798
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-30 00:40:07 -0500

    I init <TrackInfo /> component

--- components/LandingPage/TrackItem.tsx
+++ components/LandingPage/TrackItem.tsx
@@ -1,36 +1,14 @@
-import { formatEther } from "viem"
 import TrackImage from "./TrackImage"
 import AddTrackButton from "./AddTrackButton"
-import formatTime from "@/lib/formatTime"
-import useMusicDuration from "@/hooks/useMusicDuration"
+import TrackInfo from "./TrackInfo"
 import { Track } from "@/lib/types"
-import { ZORA_FEE } from "@/lib/consts"
-import getFormatSaleEnd from "@/lib/zora/getFormattedSaleEnd"
 
-const TrackItem = ({ track }: { track: Track }) => {
-  const { duration } = useMusicDuration(track?.meta?.animationUrl)
-
-  return (
-    <div className="space-y-2 relative">
-      <TrackImage track={track} />
-      <div className="space-y-2">
-        <span className="block text-black truncate text-[16px]">{track.meta.name}</span>
-        <span className="block font-size-small text-purple">
-          duration: {formatTime(duration) || "-"}
-        </span>
-        <span className="block font-size-small text-purple">
-          total sold: {track.sale.totalMinted.toString()}
-        </span>
-        <span className="block font-size-small text-purple">
-          price: {formatEther(track.sale.price + ZORA_FEE)} ETH
-        </span>
-        <span className="block font-size-small text-purple">
-          sale end: {getFormatSaleEnd(track.sale.saleEnd)}
-        </span>
-      </div>
-      <AddTrackButton track={track} />
-    </div>
-  )
-}
+const TrackItem = ({ track }: { track: Track }) => (
+  <div className="space-y-2 relative">
+    <TrackImage track={track} />
+    <TrackInfo track={track} />
+    <AddTrackButton track={track} />
+  </div>
+)
 
 export default TrackItem
